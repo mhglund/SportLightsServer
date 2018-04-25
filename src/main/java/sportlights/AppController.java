b4dd63b7db16bb5053f6df449cf1c9e443f91a7a
Madeleine <mikaelsson.madeleine@gmail.com>
2018-04-25 08:30:53 +0200

    Added pvt as origins

--- src/main/java/sportlights/AppController.java
+++ src/main/java/sportlights/AppController.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080", "http://localhost:8100"})
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080", "http://localhost:8100", "http://pvt.dsv.su.se/Group02"})
 @RestController
 public class AppController {
     private ArrayList<Field> fields;
