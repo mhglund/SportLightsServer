334bccbbac7551660f668f29c01c165ac22ab3c7
Madeleine <mikaelsson.madeleine@gmail.com>
2018-05-24 10:00:34 +0200

    Sensor working with hibernate

--- src/main/java/sportlights/Field.java
+++ src/main/java/sportlights/Field.java
@@ -8,74 +8,69 @@ import javax.persistence.Transient;
 @Entity
 public class Field implements Comparable<Field> {
 
-    @Id @GeneratedValue private Long id;
-    private String name;
-    private String visitors;
-    private boolean lights;
-    @Transient private Sensor sensor;
+  @Id @GeneratedValue private Long id;
+  private String name;
+  private String visitors;
+  private boolean lights;
+  @Transient private Sensor sensor = new Sensor(0);
 
-    public Field() {}
+  public Field() {}
 
-    public Field(String name, Long id) {
-        this.name = name;
-        this.id = id;
-        this.setVisitorAmount();
-        this.sensor = new Sensor(0);
-    }
+  public Field(String name, Long id) {
+    this.name = name;
+    this.id = id;
+    this.setVisitorAmount();
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
+  public void lightsOn() {
+    this.lights = true;
+  }
 
-    public void lightsOn() {
-        this.lights = true;
-    }
+  public void lightsOff() {
+    this.lights = false;
+  }
 
-    public void lightsOff() {
-        this.lights = false;
-    }
+  public boolean getLights() {
+    return lights;
+  }
 
-    public boolean getLights() {
-        return lights;
-    }
-    
+  public String getVisitors() {
+    return visitors = sensor.getVisitors();
+  }
 
-    public String getVisitors() {
-        return visitors;
-    }
+  public void setVisitors(String visitors) {
+    this.visitors = visitors;
+  }
 
-    public void setVisitors(String visitors) {
-        this.visitors = visitors;
-    }
+  // Metod för att sätta planens belastningsgrad.
+  private void setVisitorAmount() {
+    visitors = sensor.getVisitors();
+  }
 
-    //Metod för att sätta planens belastningsgrad.
-    private void setVisitorAmount() {
+  @Override
+  public int compareTo(Field f) {
+    int lika = this.name.compareTo(f.getName());
+    return lika;
+  }
 
-        visitors = this.sensor.getVisitors();
-
-    }
-
-    @Override
-    public int compareTo(Field f) {
-        int lika = this.name.compareTo(f.getName()); 
-        return lika;
-    }
-
-    @Override
-    public String toString() {
-        return "Field{" + "id=" + id + ", name='" + name + '\'' + '}';
-    }
+  @Override
+  public String toString() {
+    return "Field{" + "id=" + id + ", name='" + name + '\'' + '}';
+  }
 }
